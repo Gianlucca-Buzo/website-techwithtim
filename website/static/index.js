bad82ab2c970a6cd13a8d6d5a162aa76bd98f363
Gianlucca Buzo <buzo.gianlucca@scalasystems.com.br>
2021-03-15 16:38:39 -0300

    Initial commit

---
+++ website/static/index.js
@@ -0,0 +1,12 @@
+// function deleteNote(){
+//     fetch('/delete-note',{
+//         method: 'POST',
+//         body: JSON.stringify({noteId: noteId}),
+//     }).then((_res)=>{
+//         alert("Hello\nHow are you?");
+//         window.location.href = "/";
+//     })
+// }
+function deleteNote(){
+    alert("Hello\nHow are you?");
+}
