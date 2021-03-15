81b7c1ec775749ba2f5c68b449b7f9779e7005ae
Gianlucca Buzo <buzo.gianlucca@scalasystems.com.br>
2021-03-15 16:55:07 -0300

    Corrigindo js

--- website/static/index.js
+++ website/static/index.js
@@ -1,12 +1,11 @@
-// function deleteNote(){
-//     fetch('/delete-note',{
-//         method: 'POST',
-//         body: JSON.stringify({noteId: noteId}),
-//     }).then((_res)=>{
-//         alert("Hello\nHow are you?");
-//         window.location.href = "/";
-//     })
-// }
-function deleteNote(){
-    alert("Hello\nHow are you?");
-}
+ function deleteNote(){
+     fetch('/delete-note',{
+         method: 'POST',
+         body: JSON.stringify({noteId: noteId}),
+     }).then((_res)=>{
+         window.location.href = "/";
+     })
+ }
+//function deleteNote(){
+//    alert("Hello\nHow are you?");
+//}
